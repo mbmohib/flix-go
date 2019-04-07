ec49780241b4be2619f1e16cb2a008822ed0f12c
Mohammad Mohibbullah <mbmohib@gmail.com>
2019-04-07 23:01:26 +0600

    Add HeaderHeightFix

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import styled from 'styled-components';
 import Grid from '@material-ui/core/Grid';
 
@@ -7,9 +7,12 @@ import Logo from '../images/logo.svg';
 import Nav from './Nav';
 import Search from './Search';
 
-const HeaderWrapper = styled.header`
+const HeaderContainer = styled.header`
     padding: 20px 0;
     box-shadow: ${props => props.theme.largeShadow};
+    position: absolute;
+    width: 100%;
+    top: 0;
 `;
 
 const LogoWrapper = styled.div`
@@ -24,24 +27,41 @@ const HeaderRightContainer = styled.div`
     height: 100%;
 `;
 
-const Header = props => (
-    <HeaderWrapper>
-        <Container>
-            <Grid container>
-                <Grid item sm={3}>
-                    <LogoWrapper>
-                        <img src={Logo} alt="FlixGo"/>
-                    </LogoWrapper>
-                </Grid>
-                <Grid item sm={9}>
-                    <HeaderRightContainer>
-                        <Search />
-                        <Nav />
-                    </HeaderRightContainer>
-                </Grid>
-            </Grid>
-        </Container>
-    </HeaderWrapper>
-)
+class Header extends Component {
+    state = {
+        headerHeight: '50px',
+    }
+
+    headerRef = React.createRef();
+
+    componentDidMount() {
+        this.setState({ headerHeight: this.headerRef.current.clientHeight })
+    }
+
+    render() {
+        return (
+            <React.Fragment>
+                <HeaderContainer ref={this.headerRef}>
+                    <Container>
+                        <Grid container>
+                            <Grid item sm={3}>
+                                <LogoWrapper>
+                                    <img src={Logo} alt="FlixGo" />
+                                </LogoWrapper>
+                            </Grid>
+                            <Grid item sm={9}>
+                                <HeaderRightContainer>
+                                    <Search />
+                                    <Nav />
+                                </HeaderRightContainer>
+                            </Grid>
+                        </Grid>
+                    </Container>
+                </HeaderContainer>
+                <div style={{ height: this.state.headerHeight }} />
+            </React.Fragment>
+        );
+    }
+}
 
 export default Header;
